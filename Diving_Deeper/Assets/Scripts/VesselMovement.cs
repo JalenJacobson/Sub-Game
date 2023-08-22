4b1525077981540c424769fae6a60b7a59b2a202
jacobsonj <justinjacobson0118@gmail.com>
2023-08-21 21:12:51 -0600

    basic movement

--- Diving_Deeper/Assets/Scripts/VesselMovement.cs
+++ Diving_Deeper/Assets/Scripts/VesselMovement.cs
@@ -5,34 +5,63 @@ using UnityEngine.InputSystem;
 
 public class VesselMovement : MonoBehaviour
 {
+
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Keyboard.current[Key.Space].isPressed)
+        {
+            print("space");
+            rb.AddRelativeForce(transform.forward * 50);
+        }
+
+        if (Keyboard.current[Key.UpArrow].isPressed)
+        {
+            print("holding up");
+            
+            transform.Rotate(-1, 0, 0, Space.Self);
+        }
+        if (Keyboard.current[Key.DownArrow].isPressed)
+        {
+            print("holding down");
+            transform.Rotate(1, 0, 0, Space.Self);
+        }
+        if (Keyboard.current[Key.LeftArrow].isPressed)
+        {
+            print("holding left");
+            transform.Rotate(0, -1, 0, Space.Self);
+        }
+        if (Keyboard.current[Key.RightArrow].isPressed)
+        {
+            print("holding right");
+            transform.Rotate(0, 1, 0, Space.Self);
+        }
+        else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, .01f);  
     }
 
-    public void OnRotateUp(InputValue value)
-    {
-        
-        print("Up" + value);
-    }
-    public void OnRotateDown()
-    {
-        print("Down");
-    }
-    public void OnRotateLeft()
-    {
-        print("left");
-    }
-    public void OnRotateRight()
-    {
-        print("right");
-    }
+    // public void OnRotateUp()
+    // {
+    //     print("hello");
+    //     // print(context);
+    // }
+    // public void OnRotateDown()
+    // {
+    //     print("Down");
+    // }
+    // public void OnRotateLeft()
+    // {
+    //     print("left");
+    // }
+    // public void OnRotateRight()
+    // {
+    //     print("right");
+    // }
     
 }
