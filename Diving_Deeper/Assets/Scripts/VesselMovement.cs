666c845ca7effa2d49f2b9e3723c6f2ea523740d
jacobsonj <justinjacobson0118@gmail.com>
2023-08-21 22:18:36 -0600

    more

--- Diving_Deeper/Assets/Scripts/VesselMovement.cs
+++ Diving_Deeper/Assets/Scripts/VesselMovement.cs
@@ -19,29 +19,29 @@ public class VesselMovement : MonoBehaviour
         if (Keyboard.current[Key.Space].isPressed)
         {
             print("space");
-            rb.AddRelativeForce(transform.forward * 50);
+            rb.AddForce(transform.forward * 50);
         }
 
         if (Keyboard.current[Key.UpArrow].isPressed)
         {
             print("holding up");
             
-            transform.Rotate(-1, 0, 0, Space.Self);
+            transform.Rotate(1, 0, 0, Space.World);
         }
         if (Keyboard.current[Key.DownArrow].isPressed)
         {
             print("holding down");
-            transform.Rotate(1, 0, 0, Space.Self);
+            transform.Rotate(-1, 0, 0, Space.World);
         }
         if (Keyboard.current[Key.LeftArrow].isPressed)
         {
             print("holding left");
-            transform.Rotate(0, -1, 0, Space.Self);
+            transform.Rotate(0, -1, 0, Space.World);
         }
         if (Keyboard.current[Key.RightArrow].isPressed)
         {
             print("holding right");
-            transform.Rotate(0, 1, 0, Space.Self);
+            transform.Rotate(0, 1, 0, Space.World);
         }
         else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, .01f);  
     }
