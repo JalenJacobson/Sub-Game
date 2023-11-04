db57d70701ad2a1dfa31fe05ab7dd352a4a94d0f
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2023-11-04 08:53:29 -0600

    Crosshairs

--- Assets/AimCrosshairs.cs
+++ Assets/AimCrosshairs.cs
@@ -10,10 +10,18 @@ public class AimCrosshairs : MonoBehaviour
     public Camera cam;
     public float zoffset;
     public float yoffset;
+    public SpriteRenderer spriteRendererWeapon;
+    public GameObject Weapon;
+    public SpriteRenderer spriteRendererAmmo;
+    public GameObject Ammo;
+    public Sprite[] spriteArray1;
+    public Sprite[] spriteArray2;
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        spriteRendererWeapon = Weapon.GetComponent<SpriteRenderer>();
+        spriteRendererAmmo = Ammo.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,6 +35,17 @@ public class AimCrosshairs : MonoBehaviour
         // print(Input.mousePosition);
         transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y + yoffset, zoffset));
     }
+
+    public void standardAmmo()
+        {
+            spriteRendererWeapon.sprite = spriteArray1[0];
+            spriteRendererAmmo.sprite = spriteArray2[0];  
+        }
+    public void shotGunAmmo()
+        {
+            spriteRendererWeapon.sprite = spriteArray1[1];
+            spriteRendererAmmo.sprite = spriteArray2[1];
+        }
 
     
     
