14a8d4987f7ffe5b70d001057b20f1509a463231
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2023-12-07 22:04:50 -0700

    Warning

--- Assets/LifeBar.cs
+++ Assets/LifeBar.cs
@@ -8,10 +8,37 @@ public class LifeBar : MonoBehaviour
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+    public Animator anim;
+    public float Healthbar = 100;
+    public bool warningInitiated = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
     
     public void setHealth(float health)
     {
         slider.value = health;
         fill.color = gradient.Evaluate(slider.normalizedValue);
+        Healthbar = health;
+        if(Healthbar <= 30 && warningInitiated == false)
+        {
+            StartCoroutine(lowLifeWarning());
+        }
     }
+    public IEnumerator lowLifeWarning()
+    {
+        anim.Play("LowLife");
+        warningInitiated = true;
+        yield return new WaitForSeconds(5f);
+        anim.Play("RedBar");
+    }
+
+
 }
