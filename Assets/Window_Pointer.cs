ca75d36f591948b67c0639372ee2abd3164c64e9
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2024-01-03 06:03:55 -0700

    Pointer

--- Assets/Window_Pointer.cs
+++ Assets/Window_Pointer.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class Window_Pointer : MonoBehaviour
 {
-    private Vector3 target;
+
+    [SerializeField] private Camera uiCamera;
+    private Vector3 targetPosition;
     private RectTransform pointerRectTransform;
     public GameObject pointer;
     // Start is called before the first frame update
     void Awake()
     {
-        target = new Vector3(0, 0);
+        targetPosition = new Vector3(0, 0);
         pointerRectTransform = pointer.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 toPosition = target;
+        Vector3 toPosition = targetPosition;
         Vector3 fromPosition = Camera.main.transform.position;
         fromPosition.z = 0f;
         Vector3 dir = (toPosition - fromPosition).normalized;
+
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
+        bool isOffScreen = targetPositionScreenPoint.x <= 0 || targetPositionScreenPoint.x >= Screen.width || targetPositionScreenPoint.y <= 0 || targetPositionScreenPoint.y >= Screen.height;
+
+        Debug.Log(isOffScreen + " " + targetPositionScreenPoint);
+
+        if(isOffScreen)
+        {
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= 0) cappedTargetScreenPosition.x = 0f;
+            if (cappedTargetScreenPosition.x >= Screen.width) cappedTargetScreenPosition.x = Screen.width;
+            if (cappedTargetScreenPosition.y <= 0) cappedTargetScreenPosition.y = 0f;
+            if (cappedTargetScreenPosition.y >= Screen.height) cappedTargetScreenPosition.y = Screen.height;
+
+            Vector3 pointerworldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
+            pointerRectTransform.position = pointerworldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+        }
     }
     public void nextTrigger(GameObject newTrigger)
     {
-        //target = newTrigger.GetComponent<Transform>();
+        //targetPosition = newTrigger.GetComponent<Transform>();
     }
 }
