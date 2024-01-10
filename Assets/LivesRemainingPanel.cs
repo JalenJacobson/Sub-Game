5f6c241114a17089b7282d3b97de54570c327197
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2024-01-09 20:08:08 -0700

    Life UI

--- Assets/LivesRemainingPanel.cs
+++ Assets/LivesRemainingPanel.cs
@@ -7,18 +7,27 @@ public class LivesRemainingPanel : MonoBehaviour
 {
     public int livesRemaining;
     public GameObject LoseScreen;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         livesRemaining = 3;
         LoseScreen = GameObject.Find("LoseScreen");
         LoseScreen.SetActive(false);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(livesRemaining == 2)
+        {
+            anim.Play("2Lives");
+        }
+        else if(livesRemaining == 1)
+        {
+            anim.Play("1Life");
+        }
     }
 
     public void loseALife()
