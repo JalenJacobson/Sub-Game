1030010fcf8abd331e6d2e3e6bdc60fe13d652f0
jacobsonj <justinjacobson0118@gmail.com>
2024-01-27 21:39:37 -0700

    butterfly spawn

---
+++ Assets/Butterfly.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour
+{
+
+    public float angle = 0;
+    public bool clockwise;
+    public float direction = -1;
+    public float radiusOffset = 350;
+    public float circleSpeed = 3;
+    public float attackWaitTime = 3;
+    public int followHeight;
+    public GameObject followPoint;
+    public GameObject target;
+    public GameObject egg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickRandoms();
+        egg = GameObject.FindGameObjectWithTag("egg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, followPoint.transform.position, .06f);
+        angle += Time.deltaTime * direction * circleSpeed;
+        float x = Mathf.Cos(angle) * radiusOffset;
+        float y = Mathf.Sin(angle) * radiusOffset;
+        if(!followingEgg)
+        {
+            followPoint.transform.position = new Vector3(target.transform.position.x + x, target.transform.position.y + 7, target.transform.position.z + y);
+        }
+        else if(followingEgg)
+        {
+            followPoint.transform.position = new Vector3(egg.transform.position.x + x, egg.transform.position.y + 7, egg.transform.position.z + y);
+        }
+    }
+
+    public void pickRandoms()
+    {
+        radiusOffset = Random.Range(4, 60);
+        circleSpeed = Random.Range(1, 4); 
+        clockwise = Random.value > 0.5;
+        attackWaitTime = Random.Range(3, 12);
+        followHeight = Random.Range(3, 8);
+        if(clockwise)
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 1;
+        }      
+    }
+
+    public void swarmEgg()
+    {
+        StartCoroutine(followEgg());
+        print("followegg");
+    }
+
+    private bool followingEgg = false;
+
+    public IEnumerator followEgg()
+    {
+        followingEgg = true;
+        yield return new WaitForSeconds(3);
+        followingEgg = false;
+    }
+}
--- Assets/ButterflySwarm.cs
+++ Assets/ButterflySwarm.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ButterflySwarm : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject butterflyPrefab;
+    public GameObject nestCirclePoint;
+    public int spawnNumber;
+    public List<GameObject> butterflys;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "egg")
+        {
+            for(var i = 0; i< spawnNumber; i++)
+            {
+                spawnButterfly();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void spawnButterfly()
     {
-        
+        var butterfly = Instantiate(butterflyPrefab, gameObject.transform.position, Quaternion.Euler(-90, 0, 0));
+        butterfly.GetComponent<Butterfly>().target = gameObject;
+        butterflys.Add(butterfly);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("egg"))
+        {
+            foreach(GameObject butterfly in butterflys)
+            {
+                butterfly.GetComponent<Butterfly>().swarmEgg();
+            }
+        }
     }
 }
